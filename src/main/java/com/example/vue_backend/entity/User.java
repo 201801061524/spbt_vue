856299d26e466cc89de628330d751f35ce50c2d5
hannibal <1064732420@qq.com>
2023-09-15 07:34:23 +0800

    user entity表修改

--- src/main/java/com/example/vue_backend/entity/User.java
+++ src/main/java/com/example/vue_backend/entity/User.java
@@ -1,9 +1,13 @@
 package com.example.vue_backend.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName(value = "vue_table")
 public class User {
+    @TableId
     private Integer id;
     private String username;
     private String password;
