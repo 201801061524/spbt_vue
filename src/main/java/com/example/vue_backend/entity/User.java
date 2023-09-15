08448919b5f49f086027bc0bde804caa39d89004
hannibal <1064732420@qq.com>
2023-09-15 15:34:43 +0800

    add save方法，增加按钮的响应事件

--- src/main/java/com/example/vue_backend/entity/User.java
+++ src/main/java/com/example/vue_backend/entity/User.java
@@ -1,5 +1,6 @@
 package com.example.vue_backend.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -7,7 +8,7 @@ import lombok.Data;
 @Data
 @TableName(value = "vue_table")
 public class User {
-    @TableId
+    @TableId(type = IdType.AUTO)
     private Integer id;
     private String username;
     private String password;
